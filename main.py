8f0dca230acb03e834e201b9f6519be76dd735d6
Ethan J Salter <66975375+ethansalter@users.noreply.github.com>
2023-03-11 17:06:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import openai
 import time
 import os
+import json
 
 # add your openAI key as an enviornmental variable
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def ai_conversation(AI1, AI2, messageChainAI1=[], messageChainAI2=[]):
+def ai_chat(AI1, AI2, messageChainAI1=[], messageChainAI2=[]):
     if messageChainAI1 == []:
         messageChainAI1.append({"role": "system", "content": AI1.prompt})
         messageChainAI1.append({"role": "user", "content": "Hello "+AI1.name})
@@ -25,7 +26,7 @@ def ai_conversation(AI1, AI2, messageChainAI1=[], messageChainAI2=[]):
         messageChainAI1.pop(1)
     while len(messageChainAI2) > 5:
         messageChainAI2.pop(1)
-    ai_conversation(AI2, AI1, messageChainAI2, messageChainAI1)
+    ai_chat(AI2, AI1, messageChainAI2, messageChainAI1)
 
 class AI:
     def __init__(self, name, prompt):
@@ -44,7 +45,32 @@ def AIGenerate():
     time.sleep(2)
     print(f"{name} created!")
     print(2*chr(10))
-    prompt = f"you are a {title} named {name}.  You like {like}. You dislike {dislike}. Your task is to {task}."
+    prompt = f"Your task is to act like you are a {title} named {name}. You like {like}. You dislike {dislike}. Your task is to {task}."
     return AI(name, prompt)
 
-ai_conversation(AIGenerate(), AIGenerate())
+def AIRetrieve():
+    try:
+        with open(os.path.join(os.path.dirname(__file__), "personas.json"),'r') as jsonfile:
+            # First we load existing data into a dict.
+            personas = json.load(jsonfile)
+            AIRetrieveList=', '.join(personas.keys())
+    except:
+        input("Error reading file. Please reinstall program and or check for json file existance.")
+        quit()
+    while True:
+        persona=input(f"Choose a model. ({AIRetrieveList}): ")
+        if persona in personas.keys():
+            name=personas[persona]["name"]
+            title=personas[persona]["title"]
+            like=personas[persona]["like"]
+            dislike=personas[persona]["dislike"]
+            task=personas[persona]["task"]
+            prompt = f"Your task is to act like you are a {title} named {name}. You like {like}. You dislike {dislike}. Your task is to {task}."
+            return AI(name, prompt)
+        print("Please enter a valid name.")
+
+print("AI Chat")
+#Use to have custom AI chat to eachother
+ai_chat(AIGenerate(), AIGenerate())
+#Use to have premade AI chat with each other
+ai_chat(AIRetrieve(), AIRetrieve())
